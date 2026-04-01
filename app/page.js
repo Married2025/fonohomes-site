30ad317758d7bc1417de311c8febcccc36b5b19f
Married2025 <cameron@fonohomes.com>
2026-03-31 21:29:16 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,14 @@
 export default function Home() {
   return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
       <h1>Fono Homes</h1>
       <p>Luxury Real Estate in Orange County</p>
 
-      <img 
-        src="src="https://i.postimg.cc/N0m4cvrG/headshot.jpg""
+      <img
+        src="https://i.postimg.cc/N0m4cvrG/headshot.jpg"
         alt="Cameron"
-        style={{ 
-          width: "300px",
-          borderRadius: "16px",
-          marginTop: "20px"
-        }}
+        width="300"
       />
-    </main>
+    </div>
   );
 }
